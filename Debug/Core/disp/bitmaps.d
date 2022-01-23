f628d6296db28f9d2795a13cc0476b765d006600
VeryBusyBee <vph@bigmir.net>
2022-01-23 08:31:11 +0200

    Ver.0.5. Removed Debug folder

--- Debug/Core/disp/bitmaps.d
+++
@@ -1,3 +0,0 @@
-Core/disp/bitmaps.o: ../Core/disp/bitmaps.c ../Core/disp/bitmaps.h
-
-../Core/disp/bitmaps.h:
--- Debug/Core/disp/fonts.d
+++
@@ -1,3 +0,0 @@
-Core/disp/fonts.o: ../Core/disp/fonts.cpp ../Core/disp/fonts.h
-
-../Core/disp/fonts.h:
